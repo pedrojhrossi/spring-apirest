84bbb2a1d6712f5a118e23fd6a8dfcaad48aec21
pedrojhrossi <pedrojhrossi@gmail.com>
2021-02-15 11:56:45 -0400

    CRUD methods in ClienteService class

--- src/main/java/com/bolsaidedeas/spring/boot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsaidedeas/spring/boot/backend/apirest/models/services/IClienteService.java
@@ -5,7 +5,13 @@ import java.util.List;
 import com.bolsaidedeas.spring.boot.backend.apirest.models.entity.Cliente;
 
 public interface IClienteService {
-	
+
 	public List<Cliente> findAll();
-	
+
+	public Cliente findById(Long id);
+
+	public Cliente save(Cliente cliente);
+
+	public void delete(Long id);
+
 }
